da4c68f082d6c5e517e075c064bfce8250b45d37
banalok <alokkbh26@gmail.com>
2025-01-13 21:42:44 -0500

    contains to startswith

--- CDD.py
+++ CDD.py
@@ -46,7 +46,7 @@ if uploaded_file is not None:
     download_model_from_drive()
     load_model()
     org_fingerprint_df, final_df, prediction = prediction_fingerprints()
-    chem_name = org_fingerprint_df[org_fingerprint_df.apply(lambda row: row.astype(str).str.contains('CHEM').any(), axis=1)]
+    chem_name = org_fingerprint_df[org_fingerprint_df.apply(lambda row: row.astype(str).str.startswith('CHEM').any(), axis=1)]
     final_df = pd.concat([pd.Series(chem_name), pd.Series(final_df)], axis=1)
     prediction = pd.concat([pd.Series(chem_name), pd.Series(prediction)], axis=1)
     prediction = prediction.rename(columns={prediction.columns[0]: 'Molecular ID', prediction.columns[1]: 'pIC50'})
