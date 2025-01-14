0d82f897d05b9ddfce0168171fad0527a0634f1d
banalok <alokkbh26@gmail.com>
2025-01-13 21:47:33 -0500

    reshape chemname

--- CDD.py
+++ CDD.py
@@ -47,6 +47,7 @@ if uploaded_file is not None:
     load_model()
     org_fingerprint_df, final_df, prediction = prediction_fingerprints()
     chem_name = org_fingerprint_df[org_fingerprint_df.apply(lambda row: row.astype(str).str.startswith('CHEM').any(), axis=1)]
+    chem_name = chem_name.values.reshape(-1, 1)
     final_df = pd.concat([pd.Series(chem_name), pd.Series(final_df)], axis=1)
     prediction = pd.concat([pd.Series(chem_name), pd.Series(prediction)], axis=1)
     prediction = prediction.rename(columns={prediction.columns[0]: 'Molecular ID', prediction.columns[1]: 'pIC50'})
